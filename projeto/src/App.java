84f72af0bb15b7319acfcd1628ce6bfdfe807b35
Lucas Paraíso Benning de Oliveira <164495967+LucaoDez@users.noreply.github.com>
2025-11-04 18:31:05 +0000

    commit message

--- projeto/src/App.java
+++ projeto/src/App.java
@@ -19,7 +19,7 @@ public class App {
                 ((Mapa) mapaAtual).mostrarMapa();
             } else if (mapaAtual instanceof MapaLiga) {
                 ((MapaLiga) mapaAtual).mostrar();
-            } else if (mapaAtual instanceof MapaGinaoFogo) {
+            } else if (mapaAtual instanceof MapaGinasioFogo) {
                 ((MapaGinasioFogo) mapaAtual).mostrar();
             } else if (mapaAtual instanceof MapaGinasioAgua) {
                 ((MapaGinasioAgua) mapaAtual).mostrar();
